dceea8374a854a924934764cc93024891d8dcd4f
SodiqBalogun <sbalogun157@gmail.com>
2024-10-18 22:03:17 -0500

    Created a basic react project

---
+++ Project5-DataDashboard/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  let API_KEY = "c0de5759fcad4756b6c6f92e042d6716";
+
+  return (
+    <div className="App"> 
+      <h1> This is Sodiq! </h1>
+    </div>
+  )
+}
+
+export default App
