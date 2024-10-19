ed971151efe57d74a74327933d38492b82dcb63e
SodiqBalogun <sbalogun157@gmail.com>
2024-10-19 15:13:41 -0500

    Trying to get display functionality

--- Project5-DataDashboard/src/App.jsx
+++ Project5-DataDashboard/src/App.jsx
@@ -6,14 +6,18 @@ import Weather from './Components/Weather.jsx';
 const App = () => {
   let API_KEY = "c0de5759fcad4756b6c6f92e042d6716";
   let testQuery = `https://api.weatherbit.io/v2.0/current?city=Chicago&units=I&key=${API_KEY}&include=minutely`;
-  let fetchResult = "This is the starting value!";
+  let dayQuery = `https://api.weatherbit.io/v2.0/history/daily?city=Chicago&units=I&start_date=2024-10-15&end_date=2024-10-20&key=${API_KEY}`;
+
+  const [fetchResult, setFetchResult] = useState("This is the starting value!");
+  const [cityInfo, setCityInfo] = useState({cityName:"", country:"", state:""});
 
   const getInfo1 = async () => {
-    const response = await fetch(testQuery);
+    const response = await fetch(dayQuery);
     const info = await response.json();
     console.log(info);
-    console.log(info.data[0].city_name);
-    fetchResult = info.data[0].city_name;
+    console.log(info.city_name);
+    setFetchResult(info.data[0]);
+    setCityInfo(cityInfo.cityName=info.city_name, cityInfo.country=info.country_code, cityInfo.state=info.state_code);
   }
 
   useEffect (() => {
@@ -22,8 +26,16 @@ const App = () => {
 
   return (
     <div className="App"> 
+      <h1> Weather App </h1>
+      <div class="cityInfo">
+        <p> This joint is working </p>
+        <p> {cityInfo.cityName} </p>
+      </div>
+      <div class="currSummary">
+        
+      </div>
       <h1> This is Sodiq! </h1>
-      <p> {fetchResult} </p>
+      <p> {fetchResult.temp} </p>
       <Weather></Weather>
     </div>
   )
