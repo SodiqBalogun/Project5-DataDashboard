438cc6869f8b29eda2a87987b47e12980e8f9b5c
SodiqBalogun <sbalogun157@gmail.com>
2024-10-18 22:47:37 -0500

    Successfully got a API fetch call working.

--- Project5-DataDashboard/src/App.jsx
+++ Project5-DataDashboard/src/App.jsx
@@ -4,10 +4,25 @@ import './App.css';
 
 const App = () => {
   let API_KEY = "c0de5759fcad4756b6c6f92e042d6716";
+  let testQuery = `https://api.weatherbit.io/v2.0/current?city=Chicago&units=I&key=${API_KEY}&include=minutely`;
+  let fetchResult = "This is the starting value!";
+
+  const getInfo1 = async () => {
+    const response = await fetch(testQuery);
+    const info = await response.json();
+    console.log(info);
+    console.log(info.data[0].city_name);
+    fetchResult = info.data[0].city_name;
+  }
+
+  useEffect (() => {
+    getInfo1();
+  },[]);
 
   return (
     <div className="App"> 
       <h1> This is Sodiq! </h1>
+      <p> {fetchResult} </p>
     </div>
   )
 }
