c0aed6b3f379c7493de0e4287649e1463f578df7
SodiqBalogun <sbalogun157@gmail.com>
2024-10-21 14:34:01 -0500

    Got conditional rendering working as desired.

--- Project5-DataDashboard/src/App.jsx
+++ Project5-DataDashboard/src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
     setFetchResult(info.data);
     // setRevFetchResult(info.data);
     // console.log(revFetchResult);
+    console.log("This is the info: " + info);
     setCityInfo({cityName:info.city_name, country:info.country_code, state:info.state_code});
   }
 
@@ -34,32 +35,34 @@ const App = () => {
       <div className="rest">
         <div className="topBar">
           <div className="cityInfo">
-            <h2> {cityInfo.cityName} </h2>
-            <h3> {cityInfo.cityName}, {cityInfo.country}, {cityInfo.state} </h3>
+            <h2> {cityInfo.cityName !== undefined ? cityInfo.cityName : "Chicago"} </h2>
+            <h3> {cityInfo.cityName !== undefined ? cityInfo.cityName : "Chicago"}, 
+                 {cityInfo.country !== undefined ? cityInfo.country : "US"}, 
+                 {cityInfo.state !== undefined ? cityInfo.state : "IL"} </h3>
           </div>
           <div className="summaries">
             <h3> Averages for the Current Time Range </h3>
             <div className="summariesCont"> 
               <div className="currSummary">
                 <h2> Average Temperature </h2>
-                <h3> {fetchResult.length >= 1 ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.temp || 0), 0) / fetchResult.length) + " °F" : "N/A"} </h3>
+                <h3> {fetchResult !== undefined ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.temp || 0), 0) / fetchResult.length) + " °F" : "N/A"} </h3>
               </div>
               <div className="currSummary">
                 <h2> Average Relative Humidity </h2>
-                <h3> {fetchResult.length >= 1 ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.rh || 0), 0) / fetchResult.length) + "%" : "N/A"} </h3>
+                <h3> {fetchResult !== undefined ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.rh || 0), 0) / fetchResult.length) + "%" : "N/A"} </h3>
               </div>
               <div className="currSummary">
                 <h2> Average Pressure </h2>
-                <h3> {fetchResult.length >= 1 ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.pres || 0), 0) / fetchResult.length) + " mb" : "N/A"} </h3>
+                <h3> {fetchResult !== undefined ? Math.floor(fetchResult.reduce((acc, day) => acc + (day.pres || 0), 0) / fetchResult.length) + " mb" : "N/A"} </h3>
               </div>
             </div>
           </div>
         </div>
         <div className="mainFunc"> 
-          <Weather dates={fetchResult.length >= 1 ? fetchResult.map((data, index) => (<p key={index}>{data.datetime} </p>)).reverse() : "N/A"} 
-                   temps={fetchResult.length >= 1 ? fetchResult.map((data, index) => (<p key={index}>{data.temp + " °F"} </p>)).reverse() : "N/A"} 
-                   humidities={fetchResult.length >= 1 ? fetchResult.map((data, index) => (<p key={index}>{data.rh + "%"} </p>)).reverse() : "N/A"} 
-                   pressures={fetchResult.length >= 1 ? fetchResult.map((data, index) => (<p key={index}>{data.pres + " mb"} </p>)).reverse() : "N/A"}
+          <Weather dates={fetchResult !== undefined ? fetchResult.map((data, index) => (<p key={index}>{data.datetime} </p>)).reverse() : "N/A"} 
+                   temps={fetchResult !== undefined ? fetchResult.map((data, index) => (<p key={index}>{data.temp + " °F"} </p>)).reverse() : "N/A"} 
+                   humidities={fetchResult !== undefined ? fetchResult.map((data, index) => (<p key={index}>{data.rh + "%"} </p>)).reverse() : "N/A"} 
+                   pressures={fetchResult !== undefined ? fetchResult.map((data, index) => (<p key={index}>{data.pres + " mb"} </p>)).reverse() : "N/A"}
           />
         </div>
       </div>
