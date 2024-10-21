0a4051b06b9d4f748eb66af77cc6f4f2ddb928fd
SodiqBalogun <sbalogun157@gmail.com>
2024-10-21 12:20:34 -0500

    Removed irrelevant paragrah

--- Project5-DataDashboard/src/Components/Weather.jsx
+++ Project5-DataDashboard/src/Components/Weather.jsx
@@ -1,7 +1,6 @@
 const Weather = ({dates, temps, humidities, pressures} = props) => {
     return (
         <div className="Weather">
-            <p> This is my Weather Component! </p>
             <div className="weatherCols">
                 <div className="dateCol">
                     <h3> Date </h3>
